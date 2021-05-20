ffa4f206f6a49289b809d19de933857e4941da54
Adam Syed <syedadamemirputra@gmail.com>
2021-05-20 02:34:55 -0700

    Structure setup

---
+++ build/static/js/main.4ef7239c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppower-up"]=this["webpackJsonppower-up"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),o=n(3),s=n.n(o),p=(n(8),n(9),n(0));var i=function(){return Object(p.jsx)("div",{className:"App"})};s.a.render(Object(p.jsx)(r.a.StrictMode,{children:Object(p.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.4ef7239c.chunk.js.map
