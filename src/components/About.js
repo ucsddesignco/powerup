7c3ef6b85ec1f32aee2b699fe6ea878178aceb1b
Tracey <traceydu0122@gmail.com>
2021-05-22 13:49:00 -0700

    Add About Page

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,24 @@ import React from "react";
 
 function About() {
     return (
-        <div>
+        <div className="about-section">
+            <h1>What is Power Up?</h1>
+            <div className="about-text">
+                <p>
+                    Power Up is a 10-week summer design program that prepares 
+                    UCSD students for real-world innovation by pairing student 
+                    teams with BIPOC-owned and minority-owned businesses seeking 
+                    to refine their branding, redesign their website, or boost 
+                    their exposure. Each team will receive guidance from industry 
+                    mentors as they craft sustainable designs and deliverables 
+                    that’ll live beyond the scope of the project. <br /> <br />
 
+                    Our program is driven by our Diversity, Equity, and Inclusion 
+                    initiative, which is intended to support BIPOC communities and 
+                    give underrepresented design students more opportunities to 
+                    develop their skills.
+                </p>
+            </div>
         </div>
     );
 }
