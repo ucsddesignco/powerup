384b05e579fb179ee1220b0f7cb8ccbd4388ec0b
Tracey <traceydu0122@gmail.com>
2021-05-22 18:24:21 -0700

    Responsiveness

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,10 +9,10 @@ function Landing() {
         <div id="landing">
             <Navbar />
             <div className="landing-section">
-                <div id="right">
+                <div className="right" id="shop">
                     <img src={Shop} alt="Coffee Shop" />
                 </div>
-                <div id="left">
+                <div className="left" id="top">
                     <div className="top-container" id="arrow">
                         <img src={Arrow} alt="Arrow" />
                     </div>
@@ -27,13 +27,20 @@ function Landing() {
                     <div className="middle-section">
                         <img id="logo" src={DesignCo} alt="Design Co Logo" />
                         <h1 className="landing-title">Power Up</h1>
-                        <div className="text-container">
-                            <h3>SUMMER DESIGN PROGRAM</h3>
-                        </div>
-                        <div className="text-container">
-                            <h3>JUNE - SEPTEMBER 2021</h3>
-                        </div>
                     </div>
+                </div>
+                <div className="left" id="bottom">
+                    <div className="text-container">
+                        <h3>SUMMER DESIGN PROGRAM</h3>
+                    </div>
+                    <div className="text-container">
+                        <h3>JUNE - SEPTEMBER 2021</h3>
+                    </div>
+                </div>
+                <div className="mobile-landing">
+                    <a href="https://forms.gle/DpMo8EURvds47tmE8" target="_blank">
+                        Apply Here
+                    </a>
                 </div>
             </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 import Ball from "../assets/images/ball-logo.svg";
+import Menu from "../assets/images/nav-menu.svg";
+import Exit from "../assets/images/exit.svg";
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        document.body.style.overflow = menuOpen ? "visible" : "hidden" ;
+        document.documentElement.style.overflow = menuOpen ? "visible" : "hidden";
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <div className="navbar">
             <span className="nav-title">
@@ -15,6 +25,39 @@ function Navbar() {
                 <a id="button" href="https://forms.gle/DpMo8EURvds47tmE8" target="_blank">
                     Apply Here
                 </a>
+            </nav>
+
+            <nav className="mobile">
+            <img 
+                id="menu"
+                src={menuOpen ? Exit : Menu} 
+                onClick={() => { handleMenuClick() }} 
+                alt="Menu" 
+                width="30px"
+                height="30px"
+            />
+            {
+                menuOpen &&
+                <div className="nav-container">
+                    <div className="nav-item">
+                        <a href="#">Overview</a>
+                    </div>
+                    <div className="nav-item">
+                        <a href="#">Partners</a>
+                    </div>
+                    <div className="nav-item">
+                        <a href="#">Benefits</a>
+                    </div>
+                    <div className="nav-item">
+                        <a href="#">FAQ</a>
+                    </div>
+                    <div className="nav-item">
+                        <a href="https://forms.gle/DpMo8EURvds47tmE8" target="_blank">
+                            Apply
+                        </a>
+                    </div>
+                </div>
+            }
             </nav>
         </div>
     );
