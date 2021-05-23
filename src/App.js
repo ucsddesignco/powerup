e23ba0076293c3fb4929a227738072503f1a1f7d
Adam Syed <syedadamemirputra@gmail.com>
2021-05-22 18:00:30 -0700

    Cursor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
+import React, { useState } from "react";
 import FAQStructure from "./components/FAQStructure";
 import Footer from "./components/Footer";
 import WhyApply from "./components/WhyApply";
 
 function App() {
+  const [cursorX, setCursorX] = useState();
+  const [cursorY, setCursorY] = useState();
+
+  window.addEventListener("mousemove", (e) => {
+    setCursorX(e.pageX);
+    setCursorY(e.pageY);
+  });
+
   return (
     <div className="App">
+      <div
+        className="cursor"
+        style={{
+          left: cursorX + "px",
+          top: cursorY + "px",
+        }}
+      ></div>
       <WhyApply />
       <FAQStructure />
       <Footer />
