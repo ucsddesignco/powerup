f9fba245a6dacfa51b48f4e9f66374fcf6a20736
Adam Syed <syedadamemirputra@gmail.com>
2021-05-25 21:12:27 -0700

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,30 @@ import DcoLogoDark from "../assets/images/dco-logo.svg";
 function Footer() {
   return (
     <div className="footer">
+      <div className="footer-left">
+        <div className="footer-left-span">
+          <p>Made with</p>
+          <img className="footer-logo-heart" src={Heart} alt="Love Logo" />
+        </div>
+        <img
+          className="footer-logo-dco"
+          src={DcoLogoDark}
+          alt="Design Co Logo"
+        />
+      </div>
+      <div className="footer-right">
+        <a id="email" className="footer-text" href="mailto:hello@ucsddesign.co">
+          hello@ucsddesign.co
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
+
+{
+  /* <div className="footer">
       <p className="signature">
         <span className="footer-text">
           Made with
@@ -20,8 +44,5 @@ function Footer() {
       <a id="email" className="footer-text" href="mailto:hello@ucsddesign.co">
         hello@ucsddesign.co
       </a>
-    </div>
-  );
+    </div> */
 }
-
-export default Footer;
